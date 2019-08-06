24fa6c39f69784df97bf990f89c600b221c93b0e
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-08-06 16:00:32 +0200

    Add the logic so the rover can move forwards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,47 @@
+// Function to check if the rover is goinf out of limits
+const roverOutOfLimits = (rover) => {
+
+  if (rover.direction === 'N') {
+
+    if (rover.y === 0) return false
+
+  } else if (rover.direction === 'S') {
+
+    if (rover.y === 9) return false
+
+  } else if (rover.direction === 'W') {
+
+    if (rover.x === 0) return false
+    
+  } else {
+
+    if (rover.x === 9) return false
+  }
+
+  return true
+}
+
+
 // Function to move forward
 const moveForward = (rover) => {
 
   // Check if the rover can move
+  if (!roverOutOfLimits(rover)) {
+    console.log('The rover is going to roam off the map!')
+    return
+  }
+
+  // The rover can move forward
+  // Check where is heading and move
+  switch (rover.direction) {
+
+    case 'N': rover.y--; break;
+    case 'S': rover.y++; break;
+    case 'E': rover.x++; break;
+    case 'W': rover.x--; break;
+  }
+
+  console.log(rover)
 }
 
 
@@ -58,6 +98,9 @@ const executeCommands = (rover, list) => {
 // Rovers
 const rovers = {
   ironhack: {
-    direction: 'N'
+    name: "Ironhack's Mars rover",
+    direction: 'N',
+    x: 0,
+    y: 0
   }
 }
